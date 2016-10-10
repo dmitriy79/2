a852d86b30be389ccdb1a45aeb235d82ca17bf50
jleefly <jleefly@users.noreply.github.com>
2016-10-10 17:10:59 +0900

    Update addrman.cpp

--- src/addrman.cpp
+++ src/addrman.cpp
@@ -6,20 +6,6 @@
 
 using namespace std;
 
-int CAddrInfo::GetTriedBucket(const std::vector<unsigned char> &nKey) const
-{
-    CDataStream ss1(SER_GETHASH, 0);
-    std::vector<unsigned char> vchKey = GetKey();
-    ss1 << nKey << vchKey;
-    uint64_t hash1 = Hash(ss1.begin(), ss1.end()).Get64();
-
-    CDataStream ss2(SER_GETHASH, 0);
-    std::vector<unsigned char> vchGroupKey = GetGroup();
-    ss2 << nKey << vchGroupKey << (hash1 % ADDRMAN_TRIED_BUCKETS_PER_GROUP);
-    uint64_t hash2 = Hash(ss2.begin(), ss2.end()).Get64();
-    return hash2 % ADDRMAN_TRIED_BUCKET_COUNT;
-}
-
 int CAddrInfo::GetNewBucket(const std::vector<unsigned char> &nKey, const CNetAddr& src) const
 {
     CDataStream ss1(SER_GETHASH, 0);
@@ -34,25 +20,6 @@ int CAddrInfo::GetNewBucket(const std::vector<unsigned char> &nKey, const CNetAd
     return hash2 % ADDRMAN_NEW_BUCKET_COUNT;
 }
 
-bool CAddrInfo::IsTerrible(int64_t nNow) const
-{
-    if (nLastTry && nLastTry >= nNow-60) // never remove things tried the last minute
-        return false;
-
-    if (nTime > nNow + 10*60) // came in a flying DeLorean
-        return true;
-
-    if (nTime==0 || nNow-nTime > ADDRMAN_HORIZON_DAYS*86400) // not seen in over a month
-        return true;
-
-    if (nLastSuccess==0 && nAttempts>=ADDRMAN_RETRIES) // tried three times and never a success
-        return true;
-
-    if (nNow-nLastSuccess > ADDRMAN_MIN_FAIL_DAYS*86400 && nAttempts>=ADDRMAN_MAX_FAILURES) // 10 successive failures in the last week
-        return true;
-
-    return false;
-}
 
 double CAddrInfo::GetChance(int64_t nNow) const
 {
